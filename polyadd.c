78432528f20dc78447f9556b701590aae32abe7c
Sanjay S <18615926+nfsboy101@users.noreply.github.com>
2019-08-31 16:23:06 +0530

    Add files via upload

--- polyadd.c
+++ polyadd.c
@@ -19,50 +19,50 @@ struct term *createpoly(int n)
 	int i,c,e;
 	struct term *head=NULL, *nt,*last;
 	for(i=0; i<n; i++)
-		{
+	{
 		printf("Enter Coefficients and Exponents of term %d: ",i+1);
 		scanf("%d%d",&c,&e);
 		nt=createterm(c,e);
 		if(head==NULL)
 			head=last=nt;
 		else
-			{
+		{
 			last->addr=nt;
 			last=nt;
-			}
 		}
+	}
 	return head;
 };
 struct term *addpoly(struct term *h1, struct term *h2)
-	{
+{
 	struct term *rh=NULL,*p1,*p2,*n,*last;
 	p1=h1;
 	p2=h2;
 	while(p1!=NULL && p2!=NULL)
-		{
+	{
 		if(p1->exp> p2->exp)
-			{
+		{
 			n=createterm(p1->coef,p1->exp);
 			p1=p1->addr;
-			}
+		}
 		else if(p1->exp<p2->exp)
-			{
+		{
 			n=createterm(p2->coef,p2->exp);
 			p2=p2->addr;
-			}
+		}
 		else
-			{
+		{
 			n=createterm(p1->coef+p2->coef,p1->exp);
 			p1=p1->addr;
 			p2=p2->addr;
-			}
+		}
 	if(rh==NULL)
 		rh=last=n;
 	else
-		{
+	{
 		last->addr=n;
 		last=n;
-		}
+	}
 	}
 	while(p1!=NULL)
 	{
@@ -71,10 +71,10 @@ struct term *addpoly(struct term *h1, struct term *h2)
 		if(rh=NULL)
 			rh=last=n;
 		else
-			{
+		{
 			last->addr=n;
 			last=n;
-			}
+		}
 	}
 	while(p2!=NULL)
 	{
@@ -91,15 +91,15 @@ struct term *addpoly(struct term *h1, struct term *h2)
 	return rh;
 };
 void traverse(struct term *head)
-	{
+{
 	struct term*p;
 	p=head;
 	while(p!=NULL)
-		{
+	{
 		printf("%dx%d + ",p->coef,p->exp);
 		p=p->addr;
-		}
-	};
+	}
+};
 void main()
 {
 	int n1,n2;
@@ -115,4 +115,5 @@ void main()
 	p3=addpoly(p1,p2);
 	printf("\nThe sum of polynomials is : \n");
 	traverse(p3);
+	printf("\n")
 }
