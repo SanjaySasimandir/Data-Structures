6767a12f092ef9c52fbcc9a492b35dc27d97f14a
Sanjay S <18615926+nfsboy101@users.noreply.github.com>
2019-10-21 20:24:55 +0530

    Update selectionsort.c

--- selectionsort.c
+++ selectionsort.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 void main()
 {
-	int array[100],n,i,d,position,swap;
-	printf("Enter number of elements\n");
-	scanf("%d", &n);
-	printf("Enter %d integers:\n", n);
-	for(i=0;i<n;i++)
-		scanf("%d", &array[i]);
+    int a[5]={1,2,3,4,5},n=5,i,j,temp;
+    printf("Enter number of elements\n");
+    scanf("%d", &n);
+    printf("Enter %d integers:\n", n);
+    for(i=0;i<n;i++)
+        scanf("%d", &a[i]);
      
-	for(i=0; i<(n-1);i++)
-	{
-		position=i;
-	 	for(d=i+1;d<n;d++)
-		{
-			if(array[position] > array[d])
-				position= d;
-		}
-		if(position!=i)
-		{
-			swap= array[i];
-			array[i]= array[position];
-			array[position]= swap;
-		}
-	}
-	printf("Sorted list in ascending order:\n");
-	for (i = 0; i < n; i++)
-		printf("%d\n", array[i]);
-    }	
+    for(i=0; i<n;i++)
+    {
+      for(j=i+1;j<n;j++)
+      {
+        if(a[i]<a[j])
+        {
+          temp=a[i];
+          a[i]=a[j];
+          a[j]=temp;
+        }
+      }
+    }
+    printf("Sorted array is: \n");
+    for(i=0;i<n;i++)
+    {
+      printf("%d ",a[i]);
+    }
+}
