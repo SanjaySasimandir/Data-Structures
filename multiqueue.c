bad5cfd59a70819610656342330a73ca1e01c060
Sanjay S <18615926+nfsboy101@users.noreply.github.com>
2019-10-25 20:15:43 +0530

    Update multiqueue.c

--- multiqueue.c
+++ multiqueue.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int queue[10],front[5],rear[5],size=10,boundary[5],n=3;
+int queue[20],front[5],rear[5],size=20,boundary[5];
 void enqueue(int i,int data)
 {
 	if(rear[i]==boundary[i+1])
@@ -28,10 +28,12 @@ int dequeue(int i)
 };
 void main()
 {
-	int choice,data,m,i;
+	int choice,data,m,n,i;
 	boundary[0]=front[0]=rear[0]=-1;
+	printf("Enter number of queues: ");
+	scanf("%d",&n);
 	for(i=1;i<=n;i++)
-		boundary[i]=front[i]=rear[i]=(size-1)/n;
+		boundary[i]=front[i]=rear[i]=((size-1)/n)*i;
 	while(1)
 	{
 		printf("Choose from the following: \n 1. Queue \n 2. Dequeue \nEnter your choice: ");
@@ -51,4 +53,3 @@ void main()
 		}
 	}
 }
-
